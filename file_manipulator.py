6a1e0766f0ef054b1d447660e966592d3879b356
AkinoJoey <gakiya213@gmail.com>
2023-02-10 17:50:09 +0900

    copyまで作成した

--- file_manipulator.py
+++ file_manipulator.py
@@ -4,23 +4,14 @@ import os
 # 有効なコマンド
 # reverse,copy,duplicate-contents,replace-string
 
-print("実行したいコマンドを入力してください。")
-command = sys.stdin.readline().strip()
-
 def isValidCommand(command):
     if command == "reverse" or command == "copy" or command == "duplicate-contents" or command == "replace-string":
         return True
     else:
         return False
 
-def isFileExist(path):
-    if os.path.isfile(path):
-        return True
-    else:
-        return False
-
-def isDirExist(path):
-    if os.path.isdir(path):
+def isFileOrDirExsits(path):
+    if os.path.exists(path):
         return True
     else:
         return False
@@ -28,6 +19,11 @@ def isDirExist(path):
 def printAlert(missPoint):
     print("正しい" + missPoint +"を入力してください。")
 
+
+print("実行したいコマンドを入力してください。")
+command = sys.stdin.readline().strip()
+
+# コマンドのバリデータ
 while not isValidCommand(command):
     printAlert("コマンド")
     command = sys.stdin.readline().strip()
@@ -35,25 +31,49 @@ while not isValidCommand(command):
 print("入力するファイルのパスを入力してください。")
 inputPath = sys.stdin.readline().strip()
 
-while not isFileExist(inputPath):
+# inputPathのバリデータ
+while not isFileOrDirExsits(inputPath):
     printAlert("入力するファイルのパス")
     inputPath = sys.stdin.readline().strip()
 
 fileName = inputPath[inputPath.rfind('\\'):]
-print(fileName)
-print("出力するファイルのパスを入力してください。")
 
-outputPath = sys.stdin.readline().strip()
-print(outputPath + fileName)
+# コマンドがreverseだった場合　inputpath にあるファイルを受け取り、outputpath に inputpath の内容を逆にした新しいファイルを作成します。
 if command == "reverse":
-    while not isDirExist(outputPath):
+    print("出力するフォルダのパスを入力してください。")
+    outputPath = sys.stdin.readline().strip()
+
+    # outputPathがディレクトリかどうか確認するバリデータ
+    while not isFileOrDirExsits(outputPath):
         printAlert("出力するフォルダのパス")
         outputPath = sys.stdin.readline().strip()
     
     with open(inputPath) as f:
         contents = f.read()
 
-    with open(outputPath + fileName,'w') as f:
+    with open(outputPath + fileName,'x') as f:
         f.write(contents[::-1])
 
 
+# コマンドがcopyだった場合 inputpath にあるファイルのコピーを作成し、outputpath として保存します。
+if command == "copy":
+    print("出力するフォルダのパスを入力してください。")
+    outputPath = sys.stdin.readline().strip()
+
+    # outputPathがディレクトリかどうか確認するバリデータ
+    while not isFileOrDirExsits(outputPath):
+        printAlert("出力するフォルダのパス")
+        outputPath = sys.stdin.readline().strip()
+    
+    with open(inputPath) as f:
+        contents = f.read()
+
+    with open(outputPath + fileName,'x') as f:
+        f.write(contents)
+
+
+# コマンドがduplicate-contentsだった場合 inputpath にあるファイルの内容を読み込み、その内容を複製し、複製された内容を inputpath に n 回複製します
+
+
+
+# コマンドがreplace-stringだった場合　 inputpath' にあるファイルの内容から文字列 'needle' を検索し、'needle' の全てを 'newstring' に置き換えます。
