0aeca8ddc71677ce1feaaed8816f2a3e62531e65
AkinoJoey <gakiya213@gmail.com>
2023-02-11 16:21:16 +0900

    とりあえず全コマンド作成完了。ここから見直しに入る

--- file_manipulator.py
+++ file_manipulator.py
@@ -1,9 +1,6 @@
 import sys
 import os
 
-# 有効なコマンド
-# reverse,copy,duplicate-contents,replace-string
-
 def isValidCommand(command):
     if command == "reverse" or command == "copy" or command == "duplicate-contents" or command == "replace-string":
         return True
@@ -64,7 +61,6 @@ if command == "copy":
     outputPath = sys.stdin.readline().strip()
     outputPathDir = outputPath[0:outputPath.rfind('\\')] if os.name == "nt" else outputPath[0:outputPath.rfind('/'):]
 
-    print(outputPathDir)
     # outputPathDirが存在するディレクトリかどうか確認するバリデータ
     while not isFileOrDirExsits(outputPathDir):
         outputPath = sys.stdin.readline().strip()
@@ -81,7 +77,37 @@ if command == "copy":
         print("既に同じ名前のファイルが存在します。")
 
 # コマンドがduplicate-contentsだった場合 inputpath にあるファイルの内容を読み込み、その内容を複製し、複製された内容を inputpath に n 回複製します
+if command == "duplicate-contents":
+    print("複製する回数を入力してください。")
+    flag = False
 
+    while not flag:
+        copyTimes = sys.stdin.readline().strip()
 
+        if copyTimes.isnumeric() and isinstance(int(copyTimes),int):
+            flag = True
+        else:
+            print("整数を入力してください。")
+            
+    with open(inputPath) as f:
+        contents = f.read()
+
+    for i in range(int(copyTimes)):
+        with open(inputPath,'a') as f:
+            f.write(contents)
+
+    printDone(command)
+        
 
 # コマンドがreplace-stringだった場合　 inputpath' にあるファイルの内容から文字列 'needle' を検索し、'needle' の全てを 'newstring' に置き換えます。
+if command == "replace-string":
+    print("置換後の文字列を入力してください。")
+    newString = sys.stdin.readline().strip()
+
+    with open(inputPath) as f:
+        contents = f.read()
+
+    with open(inputPath,'w') as f:
+        f.write(contents.replace("needle",newString))
+
+    printDone(command)
